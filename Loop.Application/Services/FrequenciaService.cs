8e0b894e8ba7bf8f016ed2eced67b4c9683b4f19
Deyvisson-del <deyvissonrafael018@gmail.com>
2025-11-11 22:35:36 -0300

    Criação dos principais serviços

--- Loop.Application/Services/FrequenciaService.cs
+++ Loop.Application/Services/FrequenciaService.cs
@@ -1,4 +1,4 @@
-﻿using AutoMapper;
+﻿using MapsterMapper;
 using Loop.Application.DTOs;
 using Loop.Application.Interfaces;
 using Loop.Domain.Entities;
@@ -6,34 +6,34 @@ using Loop.Domain.Interfaces;
 
 namespace Loop.Application.Services
 {
-    public class FrequenciaService : IFrequenciaService
+    public class FrequenciaService : IBaseService<FrequenciaDTO>
     {
         private readonly IFrequenciaRepository _frequenciaRepository;
         private readonly IMapper _mapper;
+
         public FrequenciaService(IFrequenciaRepository frequenciaRepository, IMapper mapper)
         {
             _frequenciaRepository = frequenciaRepository;
             _mapper = mapper;
         }
 
-        public async Task<IEnumerable<FrequenciaDTO>> ObterTodasAsync()
+        public async Task<IEnumerable<FrequenciaDTO>> ObterTodosAsync()
         {
-            var entidades =  await _frequenciaRepository.ObterTodasAsync();
+            var entidades = await _frequenciaRepository.ObterTodasAsync();
             return _mapper.Map<IEnumerable<FrequenciaDTO>>(entidades);
         }
 
-
-
         public Task<IEnumerable<FrequenciaDTO>> ObterPorEstagiarioAsync(Guid estagiarioId)
         {
-            throw new NotImplementedException();
+            var entidades = _frequenciaRepository.ObterPorEstagiarioAsync(estagiarioId);
+            return _mapper.Map<Task<IEnumerable<FrequenciaDTO>>>(entidades);
         }
 
         public Task<FrequenciaDTO?> ObterPorIdAsync(Guid id)
         {
-            throw new NotImplementedException();
+            var entidade = _frequenciaRepository.ObterPorIdAsync(id);
+            return _mapper.Map<Task<FrequenciaDTO?>>(entidade);
         }
-
 
         public async Task AdicionarAsync(FrequenciaDTO dto)
         {
@@ -47,10 +47,9 @@ namespace Loop.Application.Services
             await _frequenciaRepository.AtualizarAsync(entidade);
         }
 
-     
         public async Task RemoverAsync(Guid id)
         {
-           await _frequenciaRepository.RemoverAsync(id);
+            await _frequenciaRepository.RemoverAsync(id);
         }
     }
 }
