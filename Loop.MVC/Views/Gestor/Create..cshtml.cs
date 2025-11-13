79a750677f9498e9cdde0528749c4311c36bdfb0
Deyvisson-del <deyvissonrafael018@gmail.com>
2025-11-13 18:15:53 -0300

    Mapeamento UI

---
+++ Loop.MVC/Views/Gestor/Create..cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Loop.MVC.Views.Gestor
+{
+    public class Create_Model : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
