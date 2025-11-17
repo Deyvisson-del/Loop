cf3d3d5dbf389f0f7ef9ec5ffedbbc7d6ffba0cc
Deyvisson-del <deyvissonrafael018@gmail.com>
2025-11-17 09:30:43 -0300

    Não mesclar com a master

---
+++ Loop.MVC/Controllers/EstagiarioController.cs
@@ -0,0 +1,29 @@
+﻿using Loop.Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Loop.MVC.Controllers
+{
+    public class EstagiarioController : Controller
+    {
+        private readonly IEstagiarioService _estagiarioService;
+
+        public EstagiarioController(IEstagiarioService estagiarioService)
+        {
+            _estagiarioService = estagiarioService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var estagiarios = await _estagiarioService.ObterTodosAsync();
+            return View(estagiarios);
+        }
+
+        public async Task<IActionResult> Details(Guid id)
+        {
+            var estagiario = await _estagiarioService.ObterPorIdAsync(id);
+            if (estagiario == null) return NotFound();
+
+            return View(estagiario);
+        }
+    }
+}
