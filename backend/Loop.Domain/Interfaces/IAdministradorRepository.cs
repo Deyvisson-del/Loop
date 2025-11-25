1ec0ad36b4ddfd4d81f7a117fbcb674ba6034d1f
Deyvisson-del <deyvissonrafael018@gmail.com>
2025-11-25 12:32:39 -0300

    Refatorando Interfaces Repositorys

---
+++ backend/Loop.Domain/Interfaces/IAdministradorRepository.cs
@@ -0,0 +1,11 @@
+﻿using Loop.Domain.Entities;
+
+namespace Loop.Domain.Interfaces
+{
+    public interface IAdministradorRepository
+    {
+        Task<Administrador?> ObterAdministradorPorId(int id);
+        Task<Administrador?> ObterAdminiadorPorEmail(string email);
+        Task CriarAsync(Administrador administrador);
+    }
+}
---
+++ backend/Loop.Domain/Interfaces/ISolicitacaoRepository.cs
@@ -0,0 +1,14 @@
+﻿using Loop.Domain.Entities;
+
+namespace Loop.Domain.Interfaces
+{
+    public interface ISolicitacaoRepository
+    {
+        Task<Solicitacao> CriarSolicitacaoAsync(Solicitacao solicitacao);
+        Task<Solicitacao> ObterSolicitacaoPorId(int id);
+        Task<IEnumerable<Solicitacao>> ObterPorEstagiarioIdAsync(int estagiarioId);
+        Task<IEnumerable<Solicitacao>> ObterPorPendentesAsync();
+        Task AtualizarAsync(Solicitacao);
+
+    }
+}
