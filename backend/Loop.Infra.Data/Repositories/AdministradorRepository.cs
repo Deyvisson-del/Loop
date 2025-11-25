231ad9b56d7522af49492a85f063a5a1aa6b471c
Deyvisson-del <deyvissonrafael018@gmail.com>
2025-11-25 17:34:45 -0300

    Implementação de Repository Administrador

--- backend/Loop.Infra.Data/Repositories/AdministradorRepository.cs
+++ backend/Loop.Infra.Data/Repositories/AdministradorRepository.cs
@@ -1,23 +1,55 @@
 ﻿using Loop.Domain.Entities;
 using Loop.Domain.Interfaces;
+using Loop.Infra.Data.Context;
+using Microsoft.EntityFrameworkCore;
 
 namespace Loop.Infra.Data.Repositories
 {
     public class AdministradorRepository : IAdministradorRepository
     {
-        public Task CriarAsync(Administrador administrador)
+
+        private readonly LoopDbContext _context;
+
+        public AdministradorRepository(LoopDbContext context)
         {
-            throw new NotImplementedException();
+            _context = context;
         }
 
-        public Task<Administrador?> ObterAdminiadorPorEmail(string email)
+        public async Task AtualizarAsync(Administrador administrador)
         {
-            throw new NotImplementedException();
+            _context.Administradores.Update(administrador);
+            await _context.SaveChangesAsync();
         }
 
-        public Task<Administrador?> ObterAdministradorPorId(int id)
+        public async Task CriarAsync(Administrador administrador)
         {
-            throw new NotImplementedException();
+            await _context.Administradores.AddAsync(administrador);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeletarAsync(int id)
+        {
+            var adm = await ObterAdministradorPorId(id);
+            if (adm != null)
+            {
+                _context.Administradores.Remove(adm);
+                await _context.SaveChangesAsync();
+            }
+        }
+
+        public async Task<Administrador?> ObterAdminiadorPorEmail(string email)
+        {
+           return await _context.Administradores.FirstOrDefaultAsync(x => x.Email == email);
+        }
+
+        public async Task<Administrador?> ObterAdministradorPorId(int id)
+        {
+            return await _context.Administradores.FirstOrDefaultAsync(a=>a.Id == id);
+        }
+
+        public async Task<IEnumerable<Administrador?>> ObterTodosAdministradoresAsync()
+        {
+            return await _context.Administradores.AsNoTracking().ToListAsync();
         }
     }
 }
