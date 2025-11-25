d7e4d48ba9e7dc0ecf8f1b5f7ddbb6980ee16c26
Deyvisson-del <deyvissonrafael018@gmail.com>
2025-11-25 10:35:32 -0300

    Criação das entidades solicitações e Enum de  status de solicitações.

---
+++ backend/Loop.Domain/Entities/Solicitacoes.cs
@@ -0,0 +1,63 @@
+﻿using Loop.Domain.Enums;
+
+namespace Loop.Domain.Entities
+{
+    public class Solicitacoes
+    {
+
+        public int Id { get; set; }
+        public int EstagiarioId { get; set; }
+        public Estagiario Estagiario { get; set; }
+        public int FrequenciaId { get; set; }
+        public Frequencia Frequencia { get; set; }
+        public DateTime DataSolicitacao { get; set; } = DateTime.Now;
+        public string Justificativa { get; set; }
+        public DateTime? NovaEntrada { get; set; }
+        public DateTime? NovaSaida { get; set; }
+        public StatusSolicitacao Status { get; set; } = StatusSolicitacao.PE;
+        public string? RespostaGestor { get; set; }
+        public DateTime? RespostaData { get; set; }
+
+        public Solicitacoes(){}
+
+        public Solicitacoes(int estagiarioId, int frequenciaId, string justificativa, DateTime? novaEntrada, DateTime? novaSaida)
+        {
+            EstagiarioId = estagiarioId;
+            FrequenciaId = frequenciaId;
+            Justificativa = justificativa;
+            NovaEntrada = novaEntrada;
+            NovaSaida = novaSaida;
+        }
+
+        public void Aprovar(string respostaGestor)
+        {
+            if (Status != StatusSolicitacao.PE)
+                throw new InvalidOperationException("Solicitação já foi processada. ");
+
+            Status = StatusSolicitacao.AP;
+            RespostaGestor = respostaGestor;
+            RespostaData = DateTime.Now;
+        }
+
+        public void Reprovar(string respostaGestor)
+        {
+            if (Status != StatusSolicitacao.PE)
+                throw new InvalidOperationException("Solicitação já foi processada. ");
+
+            Status = StatusSolicitacao.RP;
+            RespostaGestor = respostaGestor;
+            RespostaData = DateTime.Now;
+        }
+
+        public void AtualizarHorarios(DateTime? entrada, DateTime? saida,)
+        {
+            if (Status != StatusSolicitacao.PE)
+                throw new InvalidOperationException("Solicitação já processada.");
+
+            NovaEntrada = entrada;
+            NovaSaida = saida;
+        }
+
+
+    }
+}
---
+++ backend/Loop.Domain/Enums/StatusSolicitacao.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel;
+namespace Loop.Domain.Enums
+{
+    public enum StatusSolicitacao
+    {
+        [Description("Aprovado")] AP,
+        [Description("Reprovado")] RP,
+        [Description("Pendente")] PE,
+    }
+}
