7956cc1497724bc5672c3a47a15bd207abd3c8ea
Deyvisson-del <deyvissonrafael018@gmail.com>
2026-01-22 17:09:05 -0300

    Criação do DI da camada Infra.IoC

---
+++ Loop.Infra.IoC/DependencyInjection.cs
@@ -0,0 +1,24 @@
+﻿using Loop.Infra.Data.Context;
+using Loop.Infra.Data.Repository;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Loop.Application.UseCase.Administrador;
+using Loop.Domain.Entities;
+
+namespace Loop.Infra.IoC
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfraestructure(this  IServiceCollection services, string connectionString)
+        {
+            services.AddDbContext<Contexto>(options => options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
+
+            services.AddScoped<IAdministradorRepository, AdministradorRepository>();
+            services.AddScoped<IEstagiarioRepository, EstagiarioRepository>();
+
+
+            services.AddScoped<CriarEstagiarioUseCases>();
+            return services;
+        }
+    }
+}
