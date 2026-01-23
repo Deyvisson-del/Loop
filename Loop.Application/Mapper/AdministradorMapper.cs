d26588644f40fbd397fefe371122a8f17be9110d
Deyvisson-del <deyvissonrafael018@gmail.com>
2026-01-23 17:25:29 -0300

    Importação da biblioteca Automapper e Criação da pasta UseCase

---
+++ Loop.Application/Mapper/AdministradorMapper.cs
@@ -0,0 +1,21 @@
+﻿
+//using Loop.Domain.Entities;
+
+//namespace Loop.Application.Mapper
+//{
+//    public static class AdministradorMapper
+//    {
+//        public static Administrador ConevertendoDTOParaAdministrador(this Administrador administrador)
+//        {
+//            return new Administrador
+//            {
+//                Id = administrador.Id,
+//                Nome = administrador.Nome,
+//                Email = administrador.Email,
+//                Senha = administrador.Senha,
+//            };    
+//        }
+
+//        public static Administrador
+//    }
+//}
