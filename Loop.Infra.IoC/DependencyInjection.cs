8bc5ea36548ef442bc23c15349aedc14c09486d9
Deyvisson-del <deyvissonrafael018@gmail.com>
2026-01-27 17:05:19 -0300

    Refatora injeção de dependências de Estagiário e Gestor
    Remove casos de uso antigos e adiciona novos casos de uso com sufixos ADM e GES, separando fluxos de administradores e gestores. Atualiza namespaces para suportar as novas implementações.

--- Loop.Infra.IoC/DependencyInjection.cs
+++ Loop.Infra.IoC/DependencyInjection.cs
@@ -4,6 +4,8 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Loop.Application.UseCase.Administrador;
 using Loop.Domain.Entities;
+using Loop.Application.UseCase.Gestor;
+using Loop.Application.UseCase.EstagiarioUseCase;
 
 namespace Loop.Infra.IoC
 {
@@ -12,24 +14,24 @@ namespace Loop.Infra.IoC
         public static IServiceCollection AddInfraestructure(this IServiceCollection services, string connectionString)
         {
             services.AddDbContext<Contexto>(options => options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
-
             services.AddScoped<IAdministradorRepository, AdministradorRepository>();
             services.AddScoped<IEstagiarioRepository, EstagiarioRepository>();
             services.AddScoped<IGestorRepository, GestorRepository>();
-
-
-            services.AddScoped<CriarEstagiarioUseCase>();
-            services.AddScoped<CriarGestorUseCase>();
-
-            services.AddScoped<BuscarEstagiarioNomeUseCase>();
-            services.AddScoped<BuscarGestorNomeUseCase>();
-
-            services.AddScoped<BuscarEstagiarioIdUseCase>();
-            services.AddScoped<BuscarGestorIdUseCase>();
-
-            services.AddScoped<DeletarEstagiarioUseCase>();
-            services.AddScoped<DeletarGestorUseCase>();
-
+            services.AddScoped<CriarEstagiarioADM>();
+            services.AddScoped<CriarGestorADM>();
+            services.AddScoped<AtualizarEstagiarioADM>();
+            services.AddScoped<BuscarEstagiarioIdADM>();
+            services.AddScoped<BuscarGestorNomeADM>();
+            services.AddScoped<BuscarEstagiarioNomeADM>();
+            services.AddScoped<BuscarEstagiarioIdADM>();
+            services.AddScoped<BuscarGestorIdADM>();
+            services.AddScoped<DeletarEstagiarioADM>();
+            services.AddScoped<DeletarGestorADM>();
+            services.AddScoped<AtualizarEstagiarioGES>();
+            services.AddScoped<BuscarIdEstagiarioGES>();
+            services.AddScoped<BuscarNomeEstagiarioGES>();
+            services.AddScoped<CriarEstagiarioGES>();
+            services.AddScoped<DeletarEstagiarioGES>();
             return services;
         }
     }
