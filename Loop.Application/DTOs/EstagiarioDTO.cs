add1afc0756b38256bb90313e82245899d73a83a
Deyvisson-del <deyvissonrafael018@gmail.com>
2026-01-27 17:01:00 -0300

    Adiciona EstagiarioDTO e remove pastas do csproj
    Criada a classe EstagiarioDTO com propriedades protegidas para Id, Nome, Email e Senha. Removidas referências às pastas UseCase\Estagiario\ e UseCase\Gestor\ do Loop.Application.csproj para simplificar a estrutura do projeto.

---
+++ Loop.Application/DTOs/EstagiarioDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace Loop.Application.DTOs
+{
+    public class EstagiarioDTO
+    {
+        public int Id { get; protected set; }
+        public string Nome { get; protected set; } 
+        public string Email { get; protected set; }
+        public string Senha { get; protected set; } 
+
+    }
+}
