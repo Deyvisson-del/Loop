31eb0a479e85d34625991141da97e92934ceed20
Deyvisson-del <deyvissonrafael018@gmail.com>
2026-02-04 17:01:26 -0300

    Estrutura Do front end

---
+++ Loop.API.MVC/Controllers/Account/AccountController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Loop.API.MVC.Controllers.Account
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
