d27fbb74045fc7e570c007668c2aa7a87f897041
Deyvisson-del <deyvissonrafael018@gmail.com>
2026-02-05 17:48:57 -0300

    CRiação do DTO de frequencia

---
+++ Loop.Application/DTOs/FrequenciaDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace Loop.Application.DTOs
+{
+    public class FrequenciaDTO
+    {
+        public int EstagiarioId { get; set; }
+        public DateTime? dataHoraEntrada { get; set; }
+        public FrequenciaDTO(int estagiarioId)
+        {
+            EstagiarioId = estagiarioId;
+        }
+    }
+}
